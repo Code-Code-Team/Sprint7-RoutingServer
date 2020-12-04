5f34c8e1df2b08dd727f9eac290427b0470b00c2
E2029-Aydin <aydinsanoz2@gmail.com>
2020-12-05 00:43:48 +0300

    basic query control added

--- routes/MainRouter.js
+++ routes/MainRouter.js
@@ -3,7 +3,8 @@ const express = require('express')
 const router = express.Router()
 
 router.get("/", (req, res)=>{
-   res.render("index")
+   console.log(req.query)
+   res.render("index", {name : req.query.name})
 })
 
 module.exports = router;
