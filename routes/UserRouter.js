af9f4b894a49a2832330d9806aa231c810f247e6
E2029-Aydin <aydinsanoz2@gmail.com>
2020-12-05 00:43:08 +0300

    params reguest and respond added

--- routes/UserRouter.js
+++ routes/UserRouter.js
@@ -1,8 +1,14 @@
 const express = require('express')
 const router = express.Router()
+const data = require("../modals/data")
+
 
 router.get("/", (req, res)=>{
-   res.render("register")
+   res.render("register", {userList : data, id : ''})
 })
-
+router.get("/:id", (req, res) =>{
+   
+   const userId = req.params.id
+   res.render("register", {userList : data, id : userId})
+})
 module.exports = router;
