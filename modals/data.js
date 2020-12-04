e3a953b4b4bae0ede5ff4125d4871bc9d50b434c
E2029-Aydin <aydinsanoz2@gmail.com>
2020-12-05 00:40:44 +0300

    userList created

---
+++ modals/data.js
@@ -0,0 +1,10 @@
+const userList = [
+    {id:1, name: "Jane"},
+    {id:2, name: "John"},
+    {id:3, name: "Ed"},
+    {id:4, name: "Helen"},
+    {id:5, name: "Marry"},
+    {id:6, name: "Angelina"},
+]
+
+module.exports = userList
