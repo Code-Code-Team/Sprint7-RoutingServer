bba064957ba7a04588303a6b679fa40caf714680
E2029-Aydin <aydinsanoz2@gmail.com>
2020-12-04 19:18:30 +0300

    basic level homepage and register page routing completed

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ const UserRouter = require('./routes/UserRouter')
 const app = express();
 const port = 3000;
 
+const viewsPath = path.join(__dirname,"views") 
+
+app.set("view engine", "ejs")
+app.set("views" , viewsPath)
+
 app.use('/', MainRouter);
 
 app.use('/user', UserRouter);
--- routes/UserRouter.js
+++ routes/UserRouter.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get("/", (req, res)=>{
-   res.send("User Page")
+   res.render("user")
 })
 
 module.exports = router;
